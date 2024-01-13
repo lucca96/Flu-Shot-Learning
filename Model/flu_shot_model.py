f4e62bcacaf7de956b85ec1fa5f24d7ce241b82f
lucca96 <luccalanzellotti@gmail.com>
2024-01-13 19:22:55 -0300

    Update flu_shot_model.py

--- Model/flu_shot_model.py
+++ Model/flu_shot_model.py
@@ -3,4 +3,18 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-print("ola")
+pd.set_option("display.max_columns",100)
+
+DATA_PATH = Path.cwd().parent / "Flu-Shot-Learning" / "Data"  #creating path shortcut
+
+features_df = pd.read_csv(
+    DATA_PATH / "training_set_features.csv",
+    index_col="respondent_id"
+)
+labels_df = pd.read_csv(
+    DATA_PATH / "training_set_labels.csv",
+    index_col="respondent_id"
+)
+
+print("features_df.shape", features_df.shape)
+features_df.head()
